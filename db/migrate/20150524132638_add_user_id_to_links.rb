d4261232f335c042fd3d0ab8e641c73633393b7b
Karl Keller <kgk86@hotmail.com>
2021-01-21 23:57:11 +0100

    ActiveRecord migrations now need to be tagged with the Rails version

--- db/migrate/20150524132638_add_user_id_to_links.rb
+++ db/migrate/20150524132638_add_user_id_to_links.rb
@@ -1,4 +1,4 @@
-class AddUserIdToLinks < ActiveRecord::Migration
+class AddUserIdToLinks < ActiveRecord::Migration[4.2]
   def change
     add_column :links, :user_id, :integer
     add_index :links, :user_id
