2c8b8ce8f0691e878cd650b74e88708470ff5b91
Karl Keller <kgk86@hotmail.com>
2021-01-25 21:53:42 +0100

    fix destroy_user_session_path is triggering GET on Heroku instead of DELETE

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -251,7 +251,9 @@ Devise.setup do |config|
   # config.navigational_formats = ['*/*', :html]
 
   # The default HTTP method used to sign out a resource. Default is :delete.
-  config.sign_out_via = :delete
+  # config.sign_out_via = :delete
+  # destroy_user_session_path is triggering GET on Heroku instead of DELETE
+  config.sign_out_via = :get
 
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
