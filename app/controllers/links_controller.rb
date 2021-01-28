28c2abc4843c974f6c72e1b724d19fe006fd1904
Karl Keller <kgk86@hotmail.com>
2021-01-28 08:52:25 +0100

    Refactoring

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,5 +1,5 @@
 class LinksController < ApplicationController
-  before_action :set_link, only: [:show, :edit, :update, :destroy]
+  before_action :set_link, only: [:show, :edit, :update, :destroy, :upvote, :downvote]
   before_action :authenticate_user!, except: [:index, :show]
 
   # GET /links
@@ -63,26 +63,23 @@ class LinksController < ApplicationController
   end
 
   def upvote
-    @link = Link.find(params[:id])
     @link.upvote_by current_user
     redirect_back(fallback_location: root_path)
   end
 
   def downvote
-    @link = Link.find(params[:id])
     @link.downvote_by current_user
     redirect_back(fallback_location: root_path)
   end
 
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_link
-      @link = Link.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_link
+    @link = Link.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def link_params
-      params.require(:link).permit(:title, :url)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def link_params
+    params.require(:link).permit(:title, :url)
+  end
 end
