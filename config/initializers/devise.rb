3d17e0ac1b1aa230d0d6aac8567d05cdc085a858
Karl Keller <kgk86@hotmail.com>
2021-01-28 04:52:29 +0100

    :delete for local environment

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -253,7 +253,7 @@ Devise.setup do |config|
   # The default HTTP method used to sign out a resource. Default is :delete.
   # config.sign_out_via = :delete
   # destroy_user_session_path is triggering GET on Heroku instead of DELETE
-  config.sign_out_via = :get
+  config.sign_out_via = :delete
 
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
